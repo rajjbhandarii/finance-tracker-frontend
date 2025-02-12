cd7b6b792237a4f5de629e1a9d65c46fca082312
Raj Bhandari <230860116005@laxmi.edu.in>
2025-02-12 19:55:28 +0530

    updated route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AddTransactionComponent } from '../add-transaction/add-transaction.comp
 import { AnalyticsComponent } from '../analytics/analytics.component';
 import { DashboardComponent } from '../dash-board/dash-board.component';
 import { TransactionListComponent } from '../transaction-list/transaction-list.component';
+import { BudgetManagementComponent } from '../budget-management/budget-management.component';
 
 export const routes: Routes = [
   { path: 'add-transaction', component: AddTransactionComponent },
   { path: 'analytics', component: AnalyticsComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'transaction-list', component: TransactionListComponent },
+  { path: 'budget', component: BudgetManagementComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Default route
 ];
