d84f26378b11c0f868aa86dc84cfce22fc9c315c
Raj Bhandari <230860116005@laxmi.edu.in>
2025-02-21 19:45:53 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiDataUrl: 'https://finance-tracker-backend-1-od5m.onrender.com/api/data',
     apiBudgetUrl: 'https://finance-tracker-backend-1-od5m.onrender.com/api/budget',
-    apiUserData: 'https://finance-tracker-backend-tpfn.onrender.com/api/user'
+    apiUserData: 'https://finance-tracker-backend-1-od5m.onrender.com/api/user'
 };
